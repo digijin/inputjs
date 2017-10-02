581c9ccc6875992adc43536db9944e1bfafe11b7
digijin <james@digijin.com>
2017-10-02 21:47:15 +1100

    basics

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+module.exports = {
+	entry: "./src/index",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	resolve: {
+		modules: ["node_modules", path.resolve(__dirname, "src")]
+	},
+	devtool: "source-map",
+	target: "web",
+	devServer: {
+		proxy: {
+			// proxy URLs to backend development server
+			"/api": "http://localhost:8000"
+		},
+		contentBase: path.join(__dirname, "src") // boolean | string | array, static file location
+		// compress: true, // enable gzip compression
+		// historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+		// hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+		// https: false, // true for self-signed, object for cert authority
+		// noInfo: true, // only errors & warns on hot reload
+		// ...
+	}
+};
