a31291087d40309ad73f01f480dc99dc754e9003
digijin <james@digijin.com>
2017-10-04 15:16:06 +0100

    starting to package lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,79 @@
 const path = require("path");
-module.exports = {
-	entry: "./src/main",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js/,
-				loader: "babel-loader",
-				exclude: /node_modules/
-			},
-			{
-				test: /\.html/,
-				loader: "raw-loader",
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		modules: ["node_modules", path.resolve(__dirname, "src")]
-	},
-	devtool: "inline-source-map",
-	target: "web",
-	devServer: {
-		proxy: {
-			// proxy URLs to backend development server
-			"/api": "http://localhost:8000"
+module.exports = [
+	{
+		entry: "./src/main",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "demo.js"
 		},
-		contentBase: path.join(__dirname, "demo") // boolean | string | array, static file location
-		// compress: true, // enable gzip compression
-		// historyApiFallback: true, // true for index.html upon 404, object for multiple paths
-		// hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-		// https: false, // true for self-signed, object for cert authority
-		// noInfo: true, // only errors & warns on hot reload
-		// ...
+		module: {
+			rules: [
+				{
+					test: /\.js/,
+					loader: "babel-loader",
+					exclude: /node_modules/
+				},
+				{
+					test: /\.html/,
+					loader: "raw-loader",
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			modules: ["node_modules", path.resolve(__dirname, "src")]
+		},
+		devtool: "inline-source-map",
+		target: "web",
+		devServer: {
+			proxy: {
+				// proxy URLs to backend development server
+				"/api": "http://localhost:8000"
+			},
+			contentBase: path.join(__dirname, "demo") // boolean | string | array, static file location
+			// compress: true, // enable gzip compression
+			// historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+			// hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+			// https: false, // true for self-signed, object for cert authority
+			// noInfo: true, // only errors & warns on hot reload
+			// ...
+		}
+	},
+	{
+		entry: "./src/Input",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "unityinput.js",
+			libraryTarget: "var",
+			library: "UnityInput"
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.js/,
+					loader: "babel-loader",
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			modules: ["node_modules", path.resolve(__dirname, "src")]
+		},
+		devtool: "inline-source-map",
+		target: "web",
+		devServer: {
+			proxy: {
+				// proxy URLs to backend development server
+				"/api": "http://localhost:8000"
+			},
+			contentBase: path.join(__dirname, "demo") // boolean | string | array, static file location
+			// compress: true, // enable gzip compression
+			// historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+			// hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+			// https: false, // true for self-signed, object for cert authority
+			// noInfo: true, // only errors & warns on hot reload
+			// ...
+		}
 	}
-};
+];
