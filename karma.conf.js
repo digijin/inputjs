c296909dd5c0d26358ab8baf3bd6342bd62258aa
digijin <james@digijin.com>
2017-10-15 09:04:33 +0100

    headless chrome in docker on ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,7 @@
 // Karma configuration
+
+const isDocker = require("is-docker")();
+
 import webpackConf from "./webpack.config.js";
 delete webpackConf[0].entry;
 
@@ -9,6 +12,15 @@ module.exports = function(config) {
 		files: [{ pattern: "src/**/*karma.js", watched: false }],
 		preprocessors: {
 			"**/*karma.js": ["webpack"] //and sourcemap
+		},
+
+		customLaunchers: {
+			ChromeCI: {
+				base: "Chrome",
+				// We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+				// more permissions than Docker allows by default)
+				flags: isDocker ? ["--no-sandbox"] : []
+			}
 		},
 		webpack: webpackConf[0],
 		reporters: ["spec", "coverage"],
