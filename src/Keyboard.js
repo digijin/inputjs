43142e2cd4f2b6df1573660c7866aa1d65b64684
digijin <james@digijin.com>
2017-10-16 13:41:47 +0100

    keyboard lastAction

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -3,20 +3,27 @@
 export default class Keyboard {
 	down: Array<number>;
 	activity: { down: Array<number>, up: Array<number> };
+	lastAction: number;
 	constructor() {
 		this.activity = { down: [], up: [] };
 		this.down = [];
+		this.lastAction = 0;
 		window.onkeydown = e => {
 			//strip duplicates
+			this.action();
 			if (!this.getButton(e.keyCode)) {
 				this.down.push(e.keyCode);
 				this.activity.down.push(e.keyCode);
 			}
 		};
 		window.onkeyup = e => {
+			this.action();
 			this.down.splice(this.down.indexOf(e.keyCode), 1);
 			this.activity.up.push(e.keyCode);
 		};
+	}
+	action() {
+		this.lastAction = new Date().getTime();
 	}
 	getButton = (key: number) => {
 		return this.down.indexOf(key) > -1;
--- src/Keyboard.spec.js
+++ src/Keyboard.spec.js
@@ -12,4 +12,22 @@ describe("Keyboard unit tests", () => {
 		window.onkeyup({ keyCode: 1 });
 		expect(keyboard.getButton(1)).toBe(false);
 	});
+	describe("lastAction", () => {
+		beforeEach(() => {
+			keyboard = new Keyboard();
+		});
+		it("should have lastAction", () => {
+			expect(keyboard.lastAction).toBeDefined();
+		});
+		it("should update lastAction onkeydown", () => {
+			expect(keyboard.lastAction).toBe(0);
+			window.onkeydown({ keyCode: 1 });
+			expect(keyboard.lastAction).toBeGreaterThan(0);
+		});
+		it("should update lastAction onkeyup", () => {
+			expect(keyboard.lastAction).toBe(0);
+			window.onkeyup({ keyCode: 1 });
+			expect(keyboard.lastAction).toBeGreaterThan(0);
+		});
+	});
 });
